bfd3d593a3bb7b733c778cc2a70e62538d550664
indraagustian <56149654+indraagustian@users.noreply.github.com>
2020-06-26 15:59:41 +0700

    Add files via upload

---
+++ Arduino to Arduino I2C Communication/I2C_master/I2C_master.ino
@@ -0,0 +1,27 @@
+// I2C Master
+#include <Wire.h>
+int LED=13;
+
+int x = 0;
+
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin(); 
+  Serial.begin(9600);
+  pinMode(LED,OUTPUT);
+
+}
+
+void loop() {
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(x);              // sends x 
+  Wire.endTransmission();    // stop transmitting
+ 
+  x++; // Increment x
+  if (x > 200) x = 0; // `reset x once it gets 6
+  Serial.println(x);
+  if (x <=100) digitalWrite(LED, HIGH);
+  else digitalWrite(LED, LOW);
+   
+}
+
---
+++ Arduino to Arduino I2C Communication/I2C_slave/I2C_slave.ino
@@ -0,0 +1,27 @@
+// I2C Slave
+
+#include <Wire.h>
+int LED = 13;
+int x = 0;
+
+void setup() {
+  // Define the LED pin as Output
+  pinMode (LED, OUTPUT);
+  // Start the I2C Bus as Slave on address 9
+  Wire.begin(9); 
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+}
+
+void receiveEvent(int bytes) {
+  x = Wire.read();    // read one character from the I2C
+}
+
+void loop() {
+  //If value received is 0 blink LED for 200 ms
+  Serial.println(x);
+  if (x <=100) digitalWrite(LED, HIGH);
+  else digitalWrite(LED, LOW);
+}
+
